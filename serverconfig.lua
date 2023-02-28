9481cfa642bc1531fa0c36b19cd6e3d4662e2d19
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2023-02-28 22:00:55 +0100

    Update serverconfig.lua

--- serverconfig.lua
+++ serverconfig.lua
@@ -2,8 +2,8 @@
 --Name is the project name, for example UNSCring.txt has name ring
 --bucket is a dimension ID. 0 is the default
 PermanentScenes = {
-    {
+    --[[{
         name = 'ring',
         bucket = 0
-    }
+    }]]
 }
